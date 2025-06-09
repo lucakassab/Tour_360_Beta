3e5869b94fbdce90f2a1b3d9a21d65fd24746be3
lucakassab <48773596+lucakassab@users.noreply.github.com>
2025-06-09 16:05:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,27 @@
-// sw.js – cache-first genérico; mídias são adicionadas dinamicamente pelo loader.js
-const CACHE_CORE  = "tour360-core-v1";
-const CORE_FILES  = [
-  "index.html",
-  "manifest.json",
-  "icons/favicon.ico",
-  "icons/apple-touch-icon.png",
-  "icons/icon-192x192.png",
-  "icons/icon-512x512.png",
-  "js/loader.js",
-  "js/core.js",
-  "js/desktop.js",
-  "js/mobile.js",
-  "js/vr.js",
-  "js/VRButton.js"
+const CACHE_NAME = 'tour360-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './js/aframe.min.js',
+  './js/aframe-stereo-component.min.js',
+  './js/core.js',
+  './js/motionControllers.js',
+  './media/img_01_stereo.jpg',
+  './media/img_02_mono.jpg'
 ];
 
-// Instalamos apenas os arquivos essenciais (não inclui mídias)
-self.addEventListener("install", evt => {
+self.addEventListener('install', evt => {
   evt.waitUntil(
-    caches.open(CACHE_CORE).then(c => c.addAll(CORE_FILES))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
-// Cache-first: tenta cache, senão rede
-self.addEventListener("fetch", evt => {
+self.addEventListener('fetch', evt => {
   evt.respondWith(
-    caches.match(evt.request).then(
-      resp => resp || fetch(evt.request)
-    )
+    caches.match(evt.request)
+      .then(resp => resp || fetch(evt.request))
   );
-});
-
-// Limpa caches antigos se o nome mudar
-self.addEventListener("activate", evt => {
-  evt.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k === CACHE_CORE || k === "tour360-media-v1") ? null : caches.delete(k)))
-    )
-  );
-  self.clients.claim();
 });
